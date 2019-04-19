8710472bc5260a04e842859c79ecef59984f9364
Nathan Simpson <ndsimpson01@gmail.com>
2019-04-19 10:10:35 -0600

    Completed basic react components

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Product from '../Product/Product'
+
+
+export default class Dashboard extends Component {
+    state ={
+
+    }
+
+    render(){
+        return(
+            <div>
+                Dashboard
+                <Product/>
+            </div>
+        )
+    }
+}
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    state ={
+        
+    }
+
+    render(){
+        return(
+            <div>
+                Header
+            </div>
+        )
+    }
+}
